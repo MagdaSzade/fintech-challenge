c04d501872844c71f34642bf5b09a82346381873
MagdaSzade <magdalenasuchanek@gmail.com>
2020-09-30 18:49:10 +0200

    stash changes before checkout another branch

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+<<<<<<< HEAD
 import './globalStyles';
+=======
+import {css} from 'emotion';
+import {Button} from './common/Button';
+>>>>>>> stash changes before checkout another branch
 
 export const App = () => {
+    const test = () => {
+        console.log('?');
+    };
     return (
+<<<<<<< HEAD
         <>
             <h3 className="hover">hover me</h3>
         </>
+=======
+        <header className={header}>
+            <h3>'Good luck in developing Calculator app!'</h3>
+            <Button text="click" action={test} />
+        </header>
+>>>>>>> stash changes before checkout another branch
     );
 };
