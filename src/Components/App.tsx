ffe6a980ed31805956c79bf003cced34fa071f62
MagdaSzade <magdalenasuchanek@gmail.com>
2020-09-30 19:30:06 +0200

    CAR-15 add touch of styles to Button Component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import {css} from 'emotion';
-import {Button} from './common/Button';
 
 export const App = () => {
-    const test = () => {
-        console.log('?');
-    };
     return (
         <header className={header}>
             <h3>'Good luck in developing Calculator app!'</h3>
-            <Button text="click" action={test} />
         </header>
     );
 };
