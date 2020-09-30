8c2c1c5bbe13c1ee15b31702556815b8b08cd5cb
MagdaSzade <magdalenasuchanek@gmail.com>
2020-09-30 18:49:10 +0200

    stash changes before checkout another branch

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {css} from 'emotion';
+import {Button} from './common/Button';
 
 export const App = () => {
+    const test = () => {
+        console.log('?');
+    };
     return (
         <header className={header}>
             <h3>'Good luck in developing Calculator app!'</h3>
+            <Button text="click" action={test} />
         </header>
     );
 };
--- src/Components/common/Button.tsx
+++ src/Components/common/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {css} from 'emotion';
 
 type ButtonProps = {
     text: string;
@@ -8,8 +9,29 @@ type ButtonProps = {
 
 export const Button: React.FC<ButtonProps> = ({text, action, isDisabled}) => {
     return (
-        <button disabled={isDisabled} onClick={e => action(e)}>
+        <button className={button} disabled={isDisabled} onClick={e => action(e)}>
             {text}
         </button>
     );
 };
+
+const button = css`
+    min-width: 7rem;
+    padding: 0.3rem;
+    margin: 0.3rem;
+    background-color: #e9e7da;
+    color: #373f27;
+    border: 1px solid #3f3f27;
+    border-radius: 10px;
+    letter-spacing: 0.1rem;
+    &:hover {
+        color: gold;
+        border-color: gold;
+    }
+    &:focus {
+        outline-style: double;
+        outline-width: 0.3rem;
+        outline-color: red;
+        outline-offset: 0.1rem;
+    }
+`;
