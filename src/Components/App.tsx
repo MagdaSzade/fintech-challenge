7078df42c14a324e7b676e465c7e5c6c33965c6f
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-10-01 17:54:49 +0200

    fixed

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {css} from 'emotion';
 
-const App = () => {
-    return <div className="App"></div>;
+export const App = () => {
+    return (
+        <header className={header}>
+            <h3>Good luck in developing Calculator app!</h3>
+        </header>
+    );
 };
 
-export default App;
+const header = css({
+    color: '#1a1a1a',
+});
