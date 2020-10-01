9704dc6833cf059a323b20e92afc5a8dfe91e02e
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-10-01 18:49:27 +0200

    need help with bug

--- src/Components/common/Loader.tsx
+++ src/Components/common/Loader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import {keyframes} from '@emotion/core';
+import {css} from 'emotion';
 
 const flip = keyframes`
 0%, 100% {
@@ -17,24 +18,27 @@ const flip = keyframes`
     transform: rotateY(3600deg);
   }`;
 
-const Root = styled('div')`
-    display: inline-block;
-    transform: translateZ(1px);
-`;
+const Root: string = css({
+    display: 'inline-block',
+    transform: 'translateZ(1px)',
+});
 
-const Content = styled('div')`
-    display: inline-block;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border-radius: 50%;
-    background: #cda34f;
-    animation: ${flip} 8.4s cubic-bezier(0, 0.2, 0.8, 1) infinite;
-`;
+const Content: string = css({
+    display: 'inline-block',
+    width: '64px',
+    height: '64px',
+    margin: '8px',
+    borderRadius: '50%',
+    background: '#cda34f',
+    animation: '${flip} 8.4s cubic-bezier(0, 0.2, 0.8, 1) infinite',
+});
+
 export const Loader: React.FC = () => {
     return (
-        <Root>
-            <Content />
-        </Root>
+        <>
+            <Root>
+                <Content />
+            </Root>
+        </>
     );
 };
